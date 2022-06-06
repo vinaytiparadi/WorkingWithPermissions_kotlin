0a839a7b6e293d5787304481efb84652a4a4a9ef
Vinay Tiparadi <vinaytiparadi123@gmail.com>
2022-06-06 11:58:22 +0530

    Initial commit

---
+++ app/src/main/java/tech/vinay/workingwithpermissions/MainActivity.kt
@@ -0,0 +1,14 @@
+package tech.vinay.workingwithpermissions
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
